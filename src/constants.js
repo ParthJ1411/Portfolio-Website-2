726aa38fc5cf65c616c1e37d59c9b3e80f99aa2e
Parth Jawalekar <officialparthj@gmail.com>
2024-07-21 16:04:36 +0530

    Portfolio V2

---
+++ src/constants.js
@@ -0,0 +1,49 @@
+
+export const services = [
+    {
+        title: "Technology Enthusiast",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Software Engineer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Web Developer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+];
+
+export const name = 'Parth';
+
+export const experiences = [
+        {
+        'company': 'AICTE-EduSkills Foundation',
+        'role': 'AWS Cloud Virtual Intern',
+        'duration': 'July 2024 - Present',
+        'logo': 'https://upload.wikimedia.org/wikipedia/en/e/eb/All_India_Council_for_Technical_Education_logo.png',
+        'points': [
+            'Gain an understanding of various AWS services such as EC2, S3, Lambda, RDS, IAM, etc. Understand their use cases, benefits, and how they integrate with each other.',
+            'Learn fundamental concepts of cloud computing including scalability, elasticity, security, and cost optimization.',
+            'Engage in practical exercises or projects to apply AWS services to real-world scenarios. This could involve deploying applications, managing databases, or setting up serverless architectures.'
+        ],
+        'url': 'https://eduskillsfoundation.org/',
+    },
+    {
+        'company': 'CodeClause Inc.',
+        'role': 'Web Developer Intern',
+        'duration': 'February 2023 - March 2023',
+        'logo': 'https://codeclause.com/assets/img/Main%20Logo.png',
+        'points': [
+            'Developed a highly user-friendly Unit Converter application using HTML, CSS, and JavaScript, achieving 99% accuracy in conversions.',
+            'Created a personalized Portfolio Website that showcased my proficiency in web technologies, resulting in a 20% increase in visitor engagement.'
+        ],
+        'url': 'https://internship.codeclause.com/',
+    },
+]
+
+
+
+export const EMAIL_JS_SERVICE_ID = 'service_dwiilxb';
+export const EMAIL_JS_TEMPLATE_ID = 'template_8l87rew';
+export const EMAIL_JS_PUBLIC_KEY = 'pHPjc9RGGkF66r86N';
