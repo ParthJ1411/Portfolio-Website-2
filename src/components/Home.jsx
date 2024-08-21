c7fd28d1cebbd84783ee98a208c5eae6094f33eb
Parth Jawalekar <officialparthj@gmail.com>
2024-08-21 23:00:44 +0530

    Commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,7 +19,6 @@ const Home = () => {
 		return () => clearInterval(interval);
 	}, [text]);
 
-	
 	return (
 		<div className='area relative z-0 bg-black w-screen h-screen'>
 			<ul class="circles">
